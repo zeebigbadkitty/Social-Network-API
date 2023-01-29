7e7ed7e3929b03f5c4b4b9cd7241b19847c6f267
Socially Awkward Streams <candice.radam@gmail.com>
2023-01-28 20:19:47 -0600

    Added my gitignore and made changes in index.js.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const postRoutes = require('./userRoutes');
-const tagRoutes = require('./thoughtRoutes');
-const tagRoutes = require('./reactionRoutes');
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+const reactionRoutes = require('./reactionRoutes');
 
 router.use('/user', userRoutes);
 router.use('/thought', thoughtRoutes);
