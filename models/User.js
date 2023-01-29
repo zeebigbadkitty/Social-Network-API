208b8578047d80dcadcdc367dd69b42b74ef8ebe
Socially Awkward Streams <candice.radam@gmail.com>
2023-01-28 19:57:03 -0600

    Working on the user model.

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    username: {
+        required: true, 
+        unique: true, 
+        trim: true 
+    },
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/], 
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = userSchema;
